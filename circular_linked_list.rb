728dcfa6f8cdba081458e36e85bd08af506ca7ed
Philip Riley <hiriley510@gmail.com>
2015-02-28 17:15:56 -0800

    adds circular linked list, more linked list functions

---
+++ circular_linked_list.rb
@@ -0,0 +1,52 @@
+## Make a circular linked list
+
+class Node
+  attr_accessor :data, :pointer
+
+  def initialize(data = nil, pointer = nil)
+    @data = data
+    @pointer = pointer
+  end
+
+end
+
+class LinkedList
+  attr_accessor :head, :tail
+
+  def initialize(node)
+    @head = node
+    @tail = node
+  end
+
+  def insert(node)
+    node.pointer = @head
+    @head = node
+    @tail.pointer = @head
+  end
+
+  def print_all
+    length = 1
+    node = @head
+    while node.pointer != nil
+      node = node.pointer
+      puts node, node.data
+      length += 1
+    end
+  end
+
+end
+
+node1 = Node.new(1)
+node2 = Node.new(2)
+
+list = LinkedList.new(node1)
+list.insert(node2)
+
+p list.tail.data == 1
+p list.head.data == 2
+
+node3 = Node.new(3)
+list.insert(node3)
+
+p list.head.data == 3
+p list.tail.data == 1
--- linked_list.rb
+++ linked_list.rb
@@ -1,30 +1,105 @@
 # Implement a linked list (with insert and delete functions)
 
-## STARTED, HAVEN'T FINISHED
+## Not very DRY, put looping mechanism into a separate private method
 
 class Node
-  attr_accessor :data, :left, :right
+  attr_accessor :data, :pointer
 
-  def initialize(data)
+  def initialize(data = nil, pointer = nil)
     @data = data
+    @pointer = pointer
   end
-
-  def print_list_forward
-    node = self
-    return if node == nil
-    node.print_list_forward
-    puts node.right.data
-  end
-
-  def insert(data)
-    self.right = Node.new(data)
-  end
-
 end
 
 class LinkedList
+  attr_accessor :head, :tail
 
+  def initialize(head = nil)
+    @head = head
+  end
 
+  def insert(node) # at start
+    node.pointer = @head
+    @head = node
+  end
+
+  def remove # at start
+    @head = @head.pointer if @head != nil
+  end
+
+  def length
+    length = 1
+    node = @head
+    while node.pointer != nil
+      node = node.pointer
+      length += 1
+    end
+    return length
+  end
+
+  def print_all
+    length = 1
+    node = @head
+    while node.pointer != nil
+      node = node.pointer
+      puts node, node.data
+      length += 1
+    end
+  end
+
+  def mid
+    length = 1
+    mid = 1
+    node = @head
+    while node.pointer != nil && node.pointer.pointer != nil
+      node = node.pointer.pointer
+      length += 2
+      mid += 1
+    end
+    return mid
+  end
+
+  def find_data_at_index(index_to_find)
+    current_index = 0
+    node = @head
+    while node.pointer != nil
+      return node.data if current_index == index_to_find
+      node = node.pointer
+      current_index += 1
+    end
+    return current_index
+  end
+
+  def delete_at_index(index_to_delete)
+    current_index = 0
+    node = @head
+    while node.pointer != nil
+      if current_index + 1 == index_to_delete
+        node.pointer = node.pointer.pointer
+        return node.data
+      else
+        current_index += 1
+        node = node.pointer
+      end
+    end
+  end
 
 end
-# 5.times {|i| Node.new(i).insert(i+1); }
+
+# Make linked list
+i = 1
+list = LinkedList.new(Node.new(1))
+i += 1
+
+while i <= 20
+  list.insert(Node.new(i))
+  i += 1
+end
+
+p list.length == 20
+p list.mid == 10
+p list.find_data_at_index(3) == 17
+p list.delete_at_index(6) == 15
+p list.length == 19
+
+
